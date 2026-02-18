89d084565139959d6f1079be9f2cc728ea754bab
evoexo <dragon520111back@gmail.com>
2026-02-18 08:39:55 -0500

    Added public access modifier to methods.

--- Assignment 3/src/UniversityPersonnel.java
+++ Assignment 3/src/UniversityPersonnel.java
@@ -9,75 +9,75 @@ public abstract class UniversityPersonnel extends Notifiable {
     private int idNumber;
     private int wage; // Wage isn't cohesive but, accessor methods can be overridden in desired subclasses to throw an error
 
-    UniversityPersonnel setFirstName(String firstName) {
+    public UniversityPersonnel setFirstName(String firstName) {
         this.firstName = firstName;
         return this;
     }
 
-    UniversityPersonnel setLastName(String lastName) {
+    public UniversityPersonnel setLastName(String lastName) {
         this.lastName = lastName;
         return this;
     }
 
-    UniversityPersonnel setAddress(String address) {
+    public UniversityPersonnel setAddress(String address) {
         this.address = address;
         return this;
     }
 
-    UniversityPersonnel setEmail(String email) {
+    public UniversityPersonnel setEmail(String email) {
         this.email = email;
         return this;
     }
 
-    UniversityPersonnel setPhoneNumber(String phoneNumber) {
+    public UniversityPersonnel setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
         return this;
     }
 
-    UniversityPersonnel setAge(int age) {
+    public UniversityPersonnel setAge(int age) {
         this.age = age;
         return this;
     }
 
-    UniversityPersonnel setIdNumber(int idNumber) {
+    public UniversityPersonnel setIdNumber(int idNumber) {
         this.idNumber = idNumber;
         return this;
     }
 
-    UniversityPersonnel setWage(int wage) {
+    public UniversityPersonnel setWage(int wage) {
         this.wage = wage;
         return this;
     }
 
-    String getFirstName() {
+    public String getFirstName() {
         return this.firstName;
     }
 
-    String getLastName() {
+    public String getLastName() {
         return this.lastName;
     }
 
-    String getAddress() {
+    public String getAddress() {
         return this.address;
     }
 
-    String getEmail() {
+    public String getEmail() {
         return this.email;
     }
 
-    String getPhoneNumber() {
+    public String getPhoneNumber() {
         return this.phoneNumber;
     }
 
-    int getAge() {
+    public int getAge() {
         return this.age;
     }
 
-    int getIdNumber() {
+    public int getIdNumber() {
         return this.idNumber;
     }
 
-    int getWage() {
+    public int getWage() {
         return this.wage;
     }
 }
--- Assignment 3/src/UniversityStudent.java
+++ Assignment 3/src/UniversityStudent.java
@@ -1,7 +1,7 @@
 public class UniversityStudent extends UniversityPersonnel {
     // Specialized fields and methods for Student here (e.g., classes)
     @Override
-    int getWage() {
+    public int getWage() {
         throw new UnsupportedOperationException("Student doesn't have a wage");
     }
 }
